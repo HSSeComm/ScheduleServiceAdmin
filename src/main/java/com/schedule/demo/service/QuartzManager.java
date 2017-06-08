0f78a8c7c9dcdb98c9f74d146fd744fa564c5215
lvweibiao <122240075@qq.com>
2017-06-08 21:19:46 +0800

    add function

--- src/main/java/com/schedule/demo/service/QuartzManager.java
+++ src/main/java/com/schedule/demo/service/QuartzManager.java
@@ -2,39 +2,86 @@ package com.schedule.demo.service;
 
 import static org.quartz.CronScheduleBuilder.cronSchedule;
 
+import org.quartz.CronScheduleBuilder;
 import org.quartz.CronTrigger;
 import org.quartz.JobBuilder;
 import org.quartz.JobDataMap;
 import org.quartz.JobDetail;
+import org.quartz.JobKey;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.SchedulerFactory;
+import org.quartz.Trigger.TriggerState;
 import org.quartz.TriggerBuilder;
+import org.quartz.TriggerKey;
 import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.stereotype.Component;
 
-import com.schedule.demo.jobs.SimpleJob;
 import com.schedule.demo.vo.ScheduleJob;
 
 @Component
 public class QuartzManager {
 
 	private static SchedulerFactory gSchedulerFactory = new StdSchedulerFactory();
-	private static String JOB_GROUP_NAME = "EXTJWEB_JOBGROUP_NAME";
-	private static String TRIGGER_GROUP_NAME = "EXTJWEB_TRIGGERGROUP_NAME";
 
-	public void addJob(ScheduleJob job) throws SchedulerException {
+	public ScheduleJob addJob(ScheduleJob job) throws SchedulerException {
 		Scheduler sched = gSchedulerFactory.getScheduler();
 		JobDataMap jobDataMap = new JobDataMap();
 		jobDataMap.put("callbackUrl", "http://localhost:8080/abc");
 		JobDetail jobDetail = JobBuilder.newJob(job.getJobClass())
-				.withIdentity(job.getJobName(), job.getJobGroupName())
+				.withIdentity(job.getJobId(), job.getJobGroupName())
 				.usingJobData(jobDataMap)
 				.build();
 		CronTrigger trigger = TriggerBuilder.newTrigger().withIdentity(job.getTriggerName(), job.getTriggerGroupName())
-				.withSchedule(cronSchedule(job.getCornExpr())).forJob(job.getJobName(), job.getJobGroupName()).build();
+				.withSchedule(cronSchedule(job.getCornExpr())).forJob(job.getJobId(), job.getJobGroupName()).build();		
 		sched.scheduleJob(jobDetail,trigger);
 		sched.start();
+		
+		TriggerKey triggerKey = TriggerKey.triggerKey(job.getTriggerName(), job.getTriggerGroupName());
+		TriggerState triggerState = sched.getTriggerState(triggerKey);
+		job.setStatus(triggerState.name());
+		
+		return job;
 	}
 	
+	public void updateJobCron(ScheduleJob job) throws SchedulerException {  
+        Scheduler scheduler = gSchedulerFactory.getScheduler();  
+  
+        TriggerKey triggerKey = TriggerKey.triggerKey(job.getTriggerName(), job.getTriggerGroupName());  
+  
+        CronTrigger trigger = (CronTrigger) scheduler.getTrigger(triggerKey);  
+  
+        CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(job.getCornExpr());  
+  
+        trigger = trigger.getTriggerBuilder().withIdentity(triggerKey).withSchedule(scheduleBuilder).build();  
+  
+        scheduler.rescheduleJob(triggerKey, trigger);  
+
+    }	
+	
+	public ScheduleJob pauseJob(ScheduleJob job) throws SchedulerException {  
+		Scheduler scheduler = gSchedulerFactory.getScheduler();  
+		TriggerKey triggerKey = TriggerKey.triggerKey(job.getTriggerName(), job.getTriggerGroupName());
+	    JobKey jobKey = JobKey.jobKey(job.getJobId(), job.getJobGroupName());  
+	    scheduler.pauseJob(jobKey);  
+	    TriggerState triggerState = scheduler.getTriggerState(triggerKey);
+	    job.setStatus(triggerState.name());
+	    return job;
+	} 
+	
+	public ScheduleJob resumeJob(ScheduleJob job) throws SchedulerException {  
+        Scheduler scheduler = gSchedulerFactory.getScheduler();  
+        TriggerKey triggerKey = TriggerKey.triggerKey(job.getTriggerName(), job.getTriggerGroupName());
+        JobKey jobKey = JobKey.jobKey(job.getJobId(), job.getJobGroupName());  
+        scheduler.resumeJob(jobKey);  
+        TriggerState triggerState = scheduler.getTriggerState(triggerKey);
+	    job.setStatus(triggerState.name());
+	    return job;
+    }  
+	
+	public void deleteJob(ScheduleJob job) throws SchedulerException {  
+        Scheduler scheduler = gSchedulerFactory.getScheduler();  
+        JobKey jobKey = JobKey.jobKey(job.getJobId(), job.getJobGroupName());  
+        scheduler.deleteJob(jobKey);  
+    }
 }
--- src/test/java/com/schedule/demo/service/QuartzManagerTest.java
+++ src/test/java/com/schedule/demo/service/QuartzManagerTest.java
@@ -9,17 +9,37 @@ import com.schedule.demo.vo.ScheduleJob;
 public class QuartzManagerTest {
 	
 	@Test
-	public void testAddJob() throws SchedulerException{
+	public void testAddJob() throws SchedulerException, InterruptedException{
 		ScheduleJob job = new ScheduleJob();
-		job.setJobName("test");
+		job.setJobId("123456");
 		job.setJobClass(SimpleJob.class);
 		job.setCornExpr("*/5 * * * * ?");
 		job.setJobGroupName("test");
-		job.setTriggerGroupName("test");
-		job.setTriggerName("abc");
+		job.setTriggerGroupName("test123");
+		job.setTriggerName("456");
 		
 		QuartzManager quartzManager = new QuartzManager();
 		quartzManager.addJob(job);
+		
+		Thread.sleep(10000);
+		
+		job.setCornExpr("*/1 * * * * ?");
+		quartzManager.updateJobCron(job);
+		System.out.println(job.getStatus());
+		
+		Thread.sleep(5000);
+		
+		quartzManager.pauseJob(job);
+		System.out.println(job.getStatus());
+		
+		Thread.sleep(5000);
+		
+		quartzManager.resumeJob(job);
+		System.out.println(job.getStatus());
+		
+		Thread.sleep(5000);
+		
+		quartzManager.deleteJob(job);
 		
 		while(true){
 			
