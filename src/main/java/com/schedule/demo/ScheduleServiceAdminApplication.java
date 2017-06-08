69e062e20bd336ce4ebbb7723632dfa0b3c1f222
guiqing85 <gavenlv@foxmail.com>
2017-06-08 15:05:00 +0800

    Add springboot web

---
+++ src/main/java/com/schedule/demo/ScheduleServiceAdminApplication.java
@@ -0,0 +1,12 @@
+package com.schedule.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleServiceAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleServiceAdminApplication.class, args);
+	}
+}
